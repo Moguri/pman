5e7840cb5d7fa452e85d309093da69b4f16b9bb9
Mitchell Stokes <mogurijin@gmail.com>
2019-03-31 12:32:36 -0700

    native2bam: Fix relative texture paths

--- pman/native2bam.py
+++ pman/native2bam.py
@@ -6,9 +6,23 @@ import panda3d.core as p3d
 
 CONFIG_DATA = """
 assimp-gen-normals true
+bam-texture-mode unchanged
 """
 
 p3d.load_prc_file_data('', CONFIG_DATA)
+
+
+def make_texpath_relative(node, srcdir):
+    geomnode = node.node()
+    for idx, renderstate in enumerate(geomnode.get_geom_states()):
+        texattrib = renderstate.get_attrib(p3d.TextureAttrib)
+        if texattrib:
+            for texstage in texattrib.get_on_stages():
+                texture = texattrib.get_on_texture(texstage)
+                texture.filename = os.path.relpath(texture.filename, srcdir)
+            renderstate = renderstate.set_attrib(texattrib)
+        geomnode.set_geom_state(idx, renderstate)
+
 
 
 def main():
@@ -26,12 +40,17 @@ def main():
 
     dst.make_dir()
 
-
     loader = p3d.Loader.get_global_ptr()
     options = p3d.LoaderOptions()
     options.flags |= p3d.LoaderOptions.LF_no_cache
 
-    p3d.NodePath(loader.load_sync(src, options)).write_bam_file(dst)
+    scene = p3d.NodePath(loader.load_sync(src, options))
+
+    # Update texture paths
+    for node in scene.find_all_matches('**/+GeomNode'):
+        make_texpath_relative(node, src.get_dirname())
+
+    scene.write_bam_file(dst)
 
 
 if __name__ == '__main__':
