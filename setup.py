539822e27cc7b00edccffa3187f1a1e44e5f2f81
Mitchell Stokes <mogurijin@gmail.com>
2019-04-01 19:34:00 -0700

    Cleanup setup.py by moving things to setup.cfg

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup
 
 
-def readme():
-    with open('README.md') as readme_file:
-        return readme_file.read()
-
 __version__ = ''
 #pylint: disable=exec-used
 exec(open('pman/version.py').read())
@@ -13,29 +9,14 @@ exec(open('pman/version.py').read())
 setup(
     name='panda3d-pman',
     version=__version__,
-    description='A Python package to help bootstrap and manage Panda3D applications',
-    long_description=readme(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
     keywords='panda3d gamedev',
-    url='https://github.com/Moguri/pman',
-    author='Mitchell Stokes',
-    license='MIT',
     packages=['pman', 'pman.templates'],
-    include_package_data=True,
-    install_requires=[
-    ],
     extras_require={
         'blend2bam': ['panda3d-blend2bam >=0.5.1'],
     },
-    setup_requires=['pytest-runner'],
+    setup_requires=[
+        'pytest-runner'
+    ],
     tests_require=[
         'panda3d',
         'pytest',
