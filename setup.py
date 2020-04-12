cfbfeee240298cd9e4710e6bd12f6f8f5756b951
Mitchell Stokes <mogurijin@gmail.com>
2020-04-12 14:43:33 -0700

    setup.py: Stop making blend2bam optional
    This confused users and did not provide much benefit.

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ setup(
     version=__version__,
     keywords='panda3d gamedev',
     packages=['pman', 'pman.templates'],
-    extras_require={
-        'blend2bam': ['panda3d-blend2bam >=0.5.1'],
-    },
     setup_requires=[
         'pytest-runner'
     ],
@@ -23,13 +20,16 @@ setup(
         'pylint==2.4.*',
         'pytest-pylint',
     ],
+    install_requires=[
+        'panda3d-blend2bam >=0.5.1',
+    ],
     entry_points={
         'console_scripts': [
             'pman=pman.cli:main',
             'native2bam=pman.native2bam:main',
         ],
         'pman.converters': [
-            'blend2bam = pman.hooks:converter_blend_bam [blend2bam]',
+            'blend2bam = pman.hooks:converter_blend_bam',
             'native2bam = pman.hooks:converter_native_bam',
         ],
         'pman.renderers': [
