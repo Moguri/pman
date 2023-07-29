fe566f94673456138d7c1b5f286dd0c4d2824b06
Mitchell Stokes <mogurijin@gmail.com>
2023-07-29 16:36:15 -0700

    Add setuptools hook to automatically handle some build_apps config
    It currently does the following:
    
    * Add the export directory to include_patterns
    * Add the export directory to rename_paths (renames it to the asset directory)
    * Add a gui_apps entry for the mainfile if one is not present (uses project name as the key)

---
+++ pman/setuptools.py
@@ -0,0 +1,36 @@
+import os
+import tomli as toml
+
+def finalize_distribution_options(dist):
+    from .core import get_config
+
+    config = get_config()
+    assertdir = config['build']['asset_dir']
+    exportdir = config['build']['export_dir']
+    mainfile = config['run']['main_file']
+
+    pyproject_path = os.path.join(
+        config['internal']['projectdir'],
+        'pyproject.toml'
+    )
+
+    projectname = None
+
+    if os.path.exists(pyproject_path):
+        with open(pyproject_path, 'rb') as conffile:
+            conf = toml.load(conffile)
+
+            projectname = conf['project']['name']
+
+    cmd = dist.get_command_obj('build_apps')
+
+    # Setup export dir as the new asset dir
+    cmd.include_patterns.append(f'{exportdir}/**')
+    cmd.rename_paths[exportdir] = assertdir
+
+    # Add an application if there isn't one defined
+    if mainfile not in cmd.gui_apps.values() and projectname not in cmd.gui_apps:
+        cmd.gui_apps[projectname] = mainfile
+
+
+finalize_distribution_options.order = 100
